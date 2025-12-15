7c8c5cb33a1b1579195cd1478046cd3fbbd86461
eugenio martinez <emartinez714@gmail.com>
2025-12-14 21:09:33 -0400

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,30 @@
 #include "sort.h"
 
 /**
- * bubble_sort - compares adjacent elements and swaps them
- * @array: array
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: array to sort
  * @size: size of the array
- * Return: Void
  */
-
 void bubble_sort(int *array, size_t size)
-
 {
-	int swap;
-	size_t i, j;
+    size_t i, j;
+    int swap;
 
-	if (size < 2)
-	{
-		return;
-	}
+    if (array == NULL || size < 2)
+        return;
 
-
-	for (i = 0; i <= size; i++)
-	{
-		if (array[i + 1])
-		{
-			for (j = 0; j <= size; j++)
-			{
-				if (array[j + 1])
-				{
-					if (array[j] > array[j + 1])
-					{
-						swap = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = swap;
-						print_array(array, size);
-					}
-				}
-			}
-		}
-	}
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = swap;
+                print_array(array, size);
+            }
+        }
+    }
 }
